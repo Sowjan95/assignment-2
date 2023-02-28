2ee43a1aca15c15b23e6f2f2762e74273c939d1d
Sowjan95 <sowjanya.sritharasarma@macaulay.cuny.edu>
2023-02-27 23:11:40 -0500

    added myMap function

--- javascript-methods.js
+++ javascript-methods.js
@@ -10,7 +10,11 @@ In this Assignment, we use the prototype constructor to add new methods to the A
 
 // MAP //
 Array.prototype.myMap = function(callbackFn) {
-  // Place your code here.
+  var newArr = new Array();
+  for (let i = 0; i < this.length; i++) {
+    newArr[i] = callbackFn(array1[i]);
+  }
+  return newArr;
 };
 
 // FILTER //
